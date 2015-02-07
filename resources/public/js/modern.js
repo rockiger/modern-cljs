a5a5e7e7520ff08030ace48c962c3ca17592bd28
rockiger <marco@rockiger.com>
2015-02-08 00:20:26 +0100

    modern javascript

--- resources/public/js/modern.js
+++ resources/public/js/modern.js
@@ -43263,6 +43263,37 @@ goog.provide("modern_cljs.connect");
 goog.require("cljs.core");
 goog.require("clojure.browser.repl");
 clojure.browser.repl.connect.call(null, "http://localhost:9000/repl");
+goog.provide("modern_cljs.login");
+goog.require("cljs.core");
+modern_cljs.login.validate_form = function validate_form() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  if (cljs.core.count.call(null, email.value) > 0 && cljs.core.count.call(null, password.value) > 0) {
+    return true;
+  } else {
+    alert("Please, complete the form!");
+    return false;
+  }
+};
+modern_cljs.login.init = function init() {
+  if (cljs.core.truth_(function() {
+    var and__3760__auto__ = document;
+    if (cljs.core.truth_(and__3760__auto__)) {
+      return document.getElementById;
+    } else {
+      return and__3760__auto__;
+    }
+  }())) {
+    var login_form = document.getElementById("loginForm");
+    return login_form.onsubmit = modern_cljs.login.validate_form;
+  } else {
+    return null;
+  }
+};
+modern_cljs.login.bla = function bla() {
+  return console.log("Bla braenrai anetrianet");
+};
+window.onload = modern_cljs.login.init;
 goog.provide("modern_cljs.modern");
 goog.require("cljs.core");
 document.write("Hello, ClojerScript!");
